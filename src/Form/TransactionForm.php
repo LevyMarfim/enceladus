07e48c4e5d45086ff2002aaca3cb14d42ab73299
Levy Marfim <levymarfim@live.com>
2025-06-21 16:00:51 -0300

    chore: improve forms in transaction

--- src/Form/TransactionForm.php
+++ src/Form/TransactionForm.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,6 +20,7 @@ class TransactionForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+        // Data que liquidação
             ->add('settlement', DateType::class, [
                 'widget' => 'single_text',
                 'html5' => false,
@@ -26,10 +28,10 @@ class TransactionForm extends AbstractType
                     'data-controller' => 'datepicker',
                     'data-datepicker-alt-format-value' => 'd/M/Y',
                     'data-datepicker-alt-input-value' => true,
-                    // 'autocomplete' => 'on',
                     'placeholder' => 'dd/mm/aaaa',
                 ],
             ])
+        // Data da transação
             ->add('transaction', DateType::class, [
                 'widget' => 'single_text',
                 'html5' => false,
@@ -40,19 +42,34 @@ class TransactionForm extends AbstractType
                     'placeholder' => 'dd/mm/aaaa',
                 ],
             ])
+        // Operação de crédito ou débito
             ->add('operation', EnumType::class, [
                 'class' => OperationEnum::class,
                 'choice_label' => function (OperationEnum $operation): string {
                     return $operation->value;
                 },
+                'attr' => [
+                    'class' => 'form-select',
+                ],
             ])
+        // Tipo da operação
             ->add('type', EnumType::class, [
                 'class' => TransactionTypeEnum::class,
                 'choice_label' => function (TransactionTypeEnum $transactionType): string {
                     return $transactionType->value;
                 },
+                'attr' => [
+                    'class' => 'form-select',
+                ],
             ])
-            ->add('entry')
+        // Descrição da operação
+            ->add('entry', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Describe the entry',
+                ],
+            ])
+        // Valor da operação
             ->add('value')
             ->add('notaFiscal')
             ->add('amount')
